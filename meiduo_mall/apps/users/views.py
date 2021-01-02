4f65a114cf9b9c867a12d1ab4b1eb5c670a0a036
Gesiz <1040749858@qq.com>
2021-01-02 11:21:31 +0800

    添加子应用 视图文件

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.views import View
+
+from apps.users.models import User
+from django.http import JsonResponse
+
+
+class UsernameCountView(View):
+    # usernames/<username>/count
+    def get(self, request, username,count):
+        count = User.objects.filter(username=username).count()
+        return JsonResponse({'code': 0, 'errmsg': 'ok', 'count': count})
