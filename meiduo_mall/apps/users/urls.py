a8d443dc160c83d48409bdd7f5e603b99c614057
Gesiz <1040749858@qq.com>
2021-01-02 15:18:43 +0800

    添加注册视图

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.users.views import UsernameCountView
+from apps.users.views import UsernameCountView,RegisterView
 
 urlpatterns = [
-    path('usernames/<uc:username>/<count>/', UsernameCountView.as_view())
+    path('usernames/<uc:username>/<count>/', UsernameCountView.as_view()),
+    path('register/',RegisterView.as_view()),
 ]
