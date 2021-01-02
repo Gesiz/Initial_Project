73ae514e9f8b7356c546876c02795261482f8c4a
Gesiz <1040749858@qq.com>
2021-01-02 23:30:06 +0800

    添加手机号转换器

--- meiduo_mall/utils/converter.py
+++ meiduo_mall/utils/converter.py
@@ -8,3 +8,11 @@ class UsernameConverter:
     def to_python(self, value):
         # value 就是验证成功之后的数据
         return value
+
+class MobileConverter:
+    # 定义匹配手机号的正则表达式
+    regex = '1[3-9]\d{9}'
+
+    def to_python(self, value):
+        # to_python：将匹配结果传递到视图内部时使用
+        return value
