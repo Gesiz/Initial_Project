350c5972fa3c6d187996de65375f5eb5ae2e55b0
Gesiz <1040749858@qq.com>
2021-01-02 15:19:10 +0800

    添加注册视图

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -11,6 +11,12 @@ from django.http import JsonResponse
 
 class UsernameCountView(View):
     # usernames/<username>/count
-    def get(self, request, username,count):
+    def get(self, request, username, count):
         count = User.objects.filter(username=username).count()
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'count': count})
+
+
+class RegisterView(View):
+
+    def post(self, request):
+        pass
