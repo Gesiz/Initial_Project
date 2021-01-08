ceb0a1ad8a84618f682321d4c002a8b4000546aa
Gesiz <1040749858@qq.com>
2021-01-08 17:03:26 +0800

    添加首页查看

--- meiduo_mall/apps/goods/views.py
+++ meiduo_mall/apps/goods/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from utils.goods import *
+
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request):
+        categories = get_categories()
+
+        contents = get_contents()
+
+        context = {
+            'categories': categories,
+            'contexts': contents
+        }
+        return render(request, 'index.html', context=context)
